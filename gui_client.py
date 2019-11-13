e9d40c1399251915ef96196930a245e29c6b9ff5
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2019-11-13 10:45:00 -0800

    Add simple Tkinter gui

---
+++ gui_client.py
@@ -0,0 +1,54 @@
+import socket
+import json
+import tkinter as tk
+from tkinter import Tk, Label, Button
+from message_constants import RequestMessageID, ResponseMessageID
+
+
+class GameClient(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.connect_button = tk.Button(self, text='Connect to game server', command=self.connect_to_server)
+        self.connect_button.pack()
+
+        self.quit_button = tk.Button(self, text='QUIT', fg='red', command=self.master.destroy)
+        self.quit_button.pack()
+
+        self.info_label = tk.Label(self, text='')
+        self.info_label.pack()
+
+    def create_gameroom(self):
+        # Clear screen and deallocate all previous widgets
+        for widget in self.master.winfo_children():
+            widget.destroy()
+        # Request game list from server
+        game_list = self.get_game_list()
+        print(game_list)
+        
+    def connect_to_server(self):
+        try:
+            self.server_socket.connect(('0.0.0.0', 8080))
+            self.info_label['text'] = 'Connected!'
+            self.create_gameroom()
+        except ConnectionRefusedError:
+            self.info_label['text'] = 'Unable to connect'
+
+    def get_game_list(self):
+        self.server_socket.send(str.encode(json.dumps({
+            'id': RequestMessageID.LIST_GAMES.value,
+            'data': 'c'
+        })))
+        server_response = (self.server_socket.recv(4096)).decode("utf-8")
+        return server_response
+
+root = Tk()
+root.minsize(256, 256)
+gui = GameClient(root)
+root.mainloop()
--- message_constants.py
+++ message_constants.py
@@ -1,14 +1,27 @@
 import socket
 import json
+from enum import Enum, unique
 
 print("Message Constants Imported!")
 
 # message IDs that come from client:
+@unique
+class RequestMessageID(Enum):
+    LIST_GAMES = 0
+    CREATE_GAME = 1
+    JOIN_GAME = 2
+
 REQ_GAME_LIST = 0
 REQ_CREATE_GAME = 1
 REQ_JOIN_GAME = 2
 
 # response message IDs:
+@unique
+class ResponseMessageID(Enum):
+    NAME_TAKEN = 0
+    GAME_CREATED = 1
+    GAME_LIST = 2
+
 RES_NAME_TAKEN = 0
 RES_GAME_CREATED = 1
 RES_GAME_LIST = 2
