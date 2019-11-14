8b375aa5a8f2d9877cf410c16b313e329cf8c8b6
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2019-11-13 23:59:34 -0800

    Feature: add custom encoder/decoder for Message objects.
    Use json.dumps(message, cls=MessageEncoder) to serialize
    message objects before sending over socket.
    
    Use json.loads(serialized_message, cls=MessageDecoder) to
    deserialize a serialize message received from socket into
    a Message object.

--- app/message_constants.py
+++ app/message_constants.py
@@ -1,6 +1,7 @@
 import socket
 import json
 from enum import IntEnum, unique
+from typing import Union, Dict, List
 
 print("Message Constants Imported!")
 
@@ -21,10 +22,64 @@ class ResponseMessageID(MessageID):
     GAME_CREATED = 1
     GAME_LIST = 2
 
+
 class Message:
-    def __init__(self, message_id: MessageID, body: str):
-        self.__message_id = message_id
-        self.__body       = body
+    def __init__(self, message_id: MessageID, body: Union[str, Dict, List, None]):
+        self.message_id = message_id
+        self.body       = body
+
+
+class MessageEncoder(json.JSONEncoder):
+    """Used to serialize a message with the json.dumps function.
+    
+    To serialize a Message object, use json.dumps(msg, cls=MessageEncoder).
+    """
+    def default(self, obj: Message):
+        if not isinstance(obj, Message):
+            # Let the base class raise the TypeError
+            return json.JSONEncoder.default(self, obj)
+
+        if isinstance(obj.message_id, RequestMessageID):
+            return {
+                '__request__': {
+                    'id': obj.message_id,
+                    'body': obj.body
+                }
+            }
+
+        elif isinstance(obj.message_id, ResponseMessageID):
+            return {
+                '__response__': {
+                    'id': obj.message_id,
+                    'body': obj.body
+                }
+            }
+
+
+class MessageDecoder(json.JSONDecoder):
+    """Used to deserialize a serialized message with the json.loads function.
+
+    To deserialize a message, use json.loads(serialized, cls=MessageDecoder).
+    """
+
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.as_message, *args, **kwargs)
+
+    def as_message(self, obj):
+        if '__request__' in obj:
+            request = obj['__request__']
+            message_id = RequestMessageID(request['id'])
+            body = request['body']
+            return Message(message_id, body)
+
+        if '__response__' in obj:
+            response = obj['__response__']
+            message_id = ResponseMessageID(response['id'])
+            body = response['body']
+            return Message(message_id, body)
+
+        return obj
+            
 
 # connection - some socket object for the message to be sent through
 # id - the ID of the message to tell the receiver what the message is about, i.e. what to do with the data
--- tests/test.py
+++ tests/test.py
@@ -1,29 +1,51 @@
 import unittest
 import json
-from app.message_constants import RequestMessageID, ResponseMessageID, Message
+from app.message_constants import (
+    RequestMessageID, ResponseMessageID, Message, MessageEncoder, MessageDecoder
+)
 
 class TestRequestMessageID(unittest.TestCase):
-
     def test_is_json_serializable(self):
         try:
             json.dumps(RequestMessageID.LIST_GAMES)
-        except TypeError:
-            self.fail('RequestMessageID not JSON serializable')
+        except TypeError as error:
+            self.fail(str(error))
         
 
 class TestResponseMessageID(unittest.TestCase):
-
     def test_is_json_serializable(self):
         try:
             json.dumps(ResponseMessageID.GAME_CREATED)
-        except TypeError:
-            self.fail('ResponseMessageID not JSON serializable')
+        except TypeError as error:
+            self.fail(str(error))
 
 
-# class TestMessage(unittest.TestCase):
+class TestMessage(unittest.TestCase):
+    def test_is_json_serializable(self):
+        message_null_body = Message(RequestMessageID.CREATE_GAME, None)
+        message_list_body = Message(RequestMessageID.CREATE_GAME, [1, 2, 3, 4])
+        message_dict_body = Message(RequestMessageID.CREATE_GAME, {'x': 1, 'y': 2})
 
-#     def test_is_json_serializable(self):
-#         try:
-#             json.dumps(Message(RequestMessageID.CREATE_GAME, ''))
-#         except TypeError:
-#             self.fail('Message not JSON serializable')
+        try:
+            json.dumps(message_null_body, cls=MessageEncoder)
+            json.dumps(message_list_body, cls=MessageEncoder)
+            json.dumps(message_dict_body, cls=MessageEncoder)
+        except TypeError as error:
+            self.fail(str(error))
+
+    def test_is_json_deserializable(self):
+        request = Message(RequestMessageID.CREATE_GAME, {'name': 'my game'})
+        response = Message(ResponseMessageID.GAME_CREATED, {'name': 'game name'})
+        encoded_request = json.dumps(request, cls=MessageEncoder)
+        encoded_response = json.dumps(response, cls=MessageEncoder)
+
+        try:
+            decoded_request = json.loads(encoded_request, cls=MessageDecoder)
+            decoded_response = json.loads(encoded_response, cls=MessageDecoder)
+        except TypeError as error:
+            self.fail(str(error))
+
+        self.assertEqual(request.message_id, decoded_request.message_id)
+        self.assertEqual(request.body, decoded_request.body)
+        self.assertEqual(response.message_id, decoded_response.message_id)
+        self.assertEqual(response.body, decoded_response.body)
