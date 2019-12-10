17912115df0106a27c9b9f5113984f4dcd987120
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2019-12-10 01:25:08 -0800

    refactor: improve abstraction/encapsulation of client and gui

--- app/client.py
+++ app/client.py
@@ -2,29 +2,58 @@ import socket
 import json
 from message import RequestMessageID, ResponseMessageID, sendMSG
 
-SERVER_IP = '0.0.0.0' # IP address where the server .py is being run, assumes port 8080
+#SERVER_IP = '0.0.0.0' # IP address where the server .py is being run, assumes port 8080
+#
+#client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#print(client)
+#client.connect((SERVER_IP, 8080))
+#print("Connected to game server.")
+#while True:
+#    print("What would you like to do?")
+#    print("c - Create game\nj - Join Game")
+#    userInput = input()
+#    if userInput == 'c':
+#        print("Enter the name of your game to be shown to others:")
+#        userInput = input()
+#
+#        sendMSG(client, RequestMessageID.CREATE_GAME, userInput)
+#        from_server = (client.recv(4096)).decode("utf-8")
+#        print("reseiving ", from_server)
+#        serverMsg = json.loads(from_server)
+#        if serverMsg['id'] == ResponseMessageID.GAME_CREATED:
+#            print("Game created, waiting for player to join.")
+#        else:
+#            print("Error, game not created.")
+#    elif userInput == 'j':
+#        sendMSG(client, RequestMessageID.LIST_GAMES, "")
+#
+#client.close()
+#
+class Client():
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(client)
-client.connect((SERVER_IP, 8080))
-print("Connected to game server.")
-while True:
-    print("What would you like to do?")
-    print("c - Create game\nj - Join Game")
-    userInput = input()
-    if userInput == 'c':
-        print("Enter the name of your game to be shown to others:")
-        userInput = input()
+    def __init__(self):
+        self.serverSocket = None
+        self.peerSocket = None
 
-        sendMSG(client, RequestMessageID.CREATE_GAME, userInput)
-        from_server = (client.recv(4096)).decode("utf-8")
-        print("reseiving ", from_server)
-        serverMsg = json.loads(from_server)
-        if serverMsg['id'] == ResponseMessageID.GAME_CREATED:
-            print("Game created, waiting for player to join.")
-        else:
-            print("Error, game not created.")
-    elif userInput == 'j':
-        sendMSG(client, RequestMessageID.LIST_GAMES, "")
+    def connectToServer(self, ipAddr, port):
+        try:
+            self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.serverSocket.connect((ipAddr, port))
+        except Exception as error:
+            print(str(error))
+            self.serverSocket = None
+            raise error 
 
-client.close()
+    def connectToPeer(self, ipAddr, port):
+        pass
+
+    def closeServerConnection(self):
+        if self.serverSocket is not None:
+            self.serverSocket.close()
+
+    def closePeerConnection(self):
+        if self.peerSocket is not None:
+            self.peerSocket.close()
+
+
+
--- app/gui.py
+++ app/gui.py
@@ -1,63 +1,107 @@
+# Standard library imports
 import socket
 import json
 import tkinter as tk
 from tkinter import Tk, Label, Button
+from functools import partial
+
+# Third party library imports
+from typing import Callable
+
+# Local package imports
+from client import Client
 from message import RequestMessageID, ResponseMessageID, sendMSG
 
-class TicTacToe:
-    
-    def __init__(self):
-        self.__board = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
+class GameGUI(tk.Frame):
 
-class GameClient(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.master          = master
-        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__peer_socket   = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.master = master
+        self.client = Client()
         self.pack()
-        self.create_main_screen()
+        self.createMainScreen()
 
-    def create_main_screen(self):
-        self.connect_button = tk.Button(self, text='Connect to game server', command=self.connect_to_server)
-        self.connect_button.pack()
 
-        self.quit_button = tk.Button(self, text='QUIT', fg='red', command=self.master.destroy)
-        self.quit_button.pack()
+    def createMainScreen(self):
+        """ Creates the main screen when the GUI is launched allowing the user
+        to connect to a game server with an IP Address and Port of their choice.
+        """
+        entryFrame = tk.Frame(self)
+        ipLabel    = tk.Label(entryFrame, text='IP:')
+        portLabel  = tk.Label(entryFrame, text='Port:')
+        ipEntry    = tk.Entry(entryFrame)
+        portEntry  = tk.Entry(entryFrame)
+        entryFrame.pack()
+        ipLabel.grid(row=0, column=0)
+        ipEntry.grid(row=0, column=1)
+        portLabel.grid(row=1, column=0)
+        portEntry.grid(row=1, column=1)
 
-        self.info_label = tk.Label(self, text='')
-        self.info_label.pack()
+        buttonFrame   = tk.Frame(self)
+        connectButton = tk.Button(buttonFrame, text='Connect')
+        quitButton    = tk.Button(buttonFrame, text='QUIT', command=self.onQuit)
+        buttonFrame.pack()
+        connectButton.pack()
+        quitButton.pack()
 
-    def create_gameroom(self):
+        errorMsgFrame = tk.Frame(self)
+        errorMsgLabel = tk.Label(errorMsgFrame)
+        errorMsgFrame.pack()
+        errorMsgLabel.pack()
+
+        def onConnectButtonPressed():
+            def onSuccess():
+                self.createGameRoomScreen()
+
+            def onError(errorMsg=''):
+                errorMsgLabel['text'] = errorMsg
+
+            if ipEntry.get() and portEntry.get():
+                self.connectToGameServer(ipEntry.get(),
+                                         int(portEntry.get()),
+                                         onSuccess,
+                                         onError)
+            else:
+                onError('IP Address or Port number missing')
+
+        connectButton['command'] = onConnectButtonPressed;
+
+
+    def createGameRoomScreen(self):
+        """ Creates game room screen user sees once they have successfully
+        connected to the game server. This screen will query the server for a
+        list of available games and list them. The user will have the ability
+        to join one of the available games or play agains the game server.
+        """
         # Clear screen and deallocate all previous widgets
         for widget in self.master.winfo_children():
             widget.destroy()
-        # Request game list from server
-        game_list = self.get_game_list()
-        print(game_list)
-        
-    def connect_to_server(self):
-        try:
-            self.__server_socket.connect(('0.0.0.0', 8080))
-            self.info_label['text'] = 'Connected!'
-            self.create_gameroom()
-        except ConnectionRefusedError as error:
-            self.info_label['text'] = 'Unable to connect {}'.format(str(error))
 
-    def get_game_list(self):
-        self.__server_socket.send(str.encode(json.dumps({
-            'id': RequestMessageID.LIST_GAMES,
-            'data': 'c'
-        })))
-        server_response = (self.__server_socket.recv(4096)).decode("utf-8")
-        return server_response
+
+    def connectToGameServer(self, ipAddr: str, port: int, onSuccess: Callable, onError: Callable[str]) -> None:
+        """
+        Arguments:
+        ----------
+        ipAddr    - IP Address of game server
+        port      - Port number that game server is listening on.
+        onSuccess - Callback to be called if connection is successfull.
+        onError   - Callback to be called if connection fails.
+        """
+        try:
+            self.client.connectToServer(ipAddr, port)
+        except Exception as error:
+            onError(str(error))
+        else:
+            onSuccess()
+
+    def onQuit(self):
+        """ Close all socket connections and close GUI. """
+        self.client.closeServerConnection()
+        self.client.closePeerConnection()
+        self.master.destroy()
 
 if __name__ == '__main__':
     root = Tk()
     root.minsize(256, 256)
-    gui = GameClient(root)
+    gui = GameGUI(root)
     root.mainloop()
