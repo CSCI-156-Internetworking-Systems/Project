d45b1dec0c5007956a79155609919c615642ae90
roberthov <48002190+roberthov@users.noreply.github.com>
2019-12-10 18:56:18 -0800

    Update to Game Logic interface

--- app/game_logic.py
+++ app/game_logic.py
@@ -1,33 +1,44 @@
 #from os import system
 class TicTacToe:
     
-	def __init__(self):
+	def __init__(self,myNewName,newOpponentsName,startingTurnPlayer):
 		self.__board = [
 			[0, 0, 0],
 			[0, 0, 0],
 			[0, 0, 0]
 		]
-		self.__turnPlayer = 1
-	def __str__(self):
-		ret = ""
-		for row in self.__board:
-			ret += str(row[0]) + str(row[1]) + str(row[2])
-		return ret[:-1]
-	def print(self):
-		ret = ""
-		for row in self.__board:
-			ret += "X" if row[0] == 1 else ("O" if row[0] == 2 else "-")
-			ret += "X" if row[1] == 1 else ("O" if row[1] == 2 else "-")
-			ret += "X" if row[2] == 1 else ("O" if row[2] == 2 else "-")
-			ret += "\n"
-		ret = ret[:-1]
-		print(ret)
-	def makeMove(self, index):
+        self.__myName = myNewName
+        self.__opName = newOpponentsName
+		self.__turnPlayer = startingTurnPlayer
+#	def __str__(self):
+#		ret = ""
+#		for row in self.__board:
+#			ret += str(row[0]) + str(row[1]) + str(row[2])
+#		return ret[:-1]
+#	def print(self):
+#		ret = ""
+#		for row in self.__board:
+#			ret += "X" if row[0] == 1 else ("O" if row[0] == 2 else "-")
+#			ret += "X" if row[1] == 1 else ("O" if row[1] == 2 else "-")
+#			ret += "X" if row[2] == 1 else ("O" if row[2] == 2 else "-")
+#			ret += "\n"
+#		ret = ret[:-1]
+#		print(ret)
+	def getBoard(self):
+		return self.__board
+	def makeMove(self, position, attemptedTurnPlayer):
+		if (self.__turnPlayer == attemptedTurnPlayer):
+			return False
+#		index = (3 * position.x) + position.y
 		if (index < 0) | (index > 8):
-			return
-		if self.__board[int(index / 3)][index % 3] == 0:
-			self.__board[int(index / 3)][index % 3] = self.__turnPlayer
-			self.__turnPlayer = 1 if self.__turnPlayer == 2 else 2
+			return False
+#		if self.__board[int(index / 3)][index % 3] == 0:
+#			self.__board[int(index / 3)][index % 3] = self.__turnPlayer
+		if self.__board[position.x][position.y] == 0:
+			self.__board[position.x][position.x] = self.__turnPlayer
+			self.__turnPlayer = self.__myName if self.__turnPlayer == self.__opName else self.__opName
+			return True
+		return False
 	def getTurnPlayer(self):
 		return self.__turnPlayer
 	def checkWinCondition(self):
@@ -41,17 +52,19 @@ class TicTacToe:
 		return 0
 
 
-#board = TicTacToe()
+#board = TicTacToe(1)
 ##board.makeMove(0,"X")
 #system('clear')
 ##print(board)
-#board.print()
+##board.print()
+#print(board.getBoard())
 #while 1:
 #	index = int(input())
-#	board.makeMove(index)
+#	print("The move was:", "Successful" if board.makeMove(index) else "Unsuccessful")
 #	system('clear')
 ##	print(board)
-#	board.print()
+##	board.print()
+#	print(board.getBoard())
 #	print("Turn Player is: ", board.getTurnPlayer())
 #	currentWinner = board.checkWinCondition()
 #	print("The winner is: ", 'X' if currentWinner == 1 else 'O' if currentWinner == 2 else "No one")
